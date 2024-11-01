fa60f210bfcbc6dae3a2d2ada026a2d642afdd6a
Lucas_bq <lucasbequis84@gmail.com>
2024-11-01 08:39:44 -0300

    ae
    ea

---
+++ JS/ojo.js
@@ -0,0 +1,14 @@
+function verpassword() {
+    const password = document.querySelector("#Password");
+    const eyeIcon = document.querySelector(".fa");
+
+    if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    } else {
+        password.type = "password"; 
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    }
+}
